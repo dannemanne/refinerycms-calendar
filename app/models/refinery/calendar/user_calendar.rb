5ec19d80df85984645d5f7b2c4cffdd7b574bba1
Daniel Wiklund <daniel@happyrabbit.com>
2014-09-26 17:46:03 +0800

    Added calender model

--- app/models/refinery/calendar/user_calendar.rb
+++ app/models/refinery/calendar/user_calendar.rb
@@ -8,7 +8,7 @@ module Refinery
       validates :calendar_id,   presence: true, uniqueness: { scope: :user_id }
 
       before_create do
-        self.rgb_code ||= calendar.try(:default_rgb_cdoe)
+        self.rgb_code ||= calendar.try(:default_rgb_code)
       end
 
     end
