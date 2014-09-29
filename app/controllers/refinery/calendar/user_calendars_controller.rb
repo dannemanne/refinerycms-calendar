b9b7319adda0706e79ffe6538b4e09895d8486da
Daniel Wiklund <daniel@happyrabbit.com>
2014-09-29 10:44:18 +0800

    added user calendar controller

--- app/controllers/refinery/calendar/user_calendars_controller.rb
+++ app/controllers/refinery/calendar/user_calendars_controller.rb
@@ -6,9 +6,9 @@ module Refinery
         @user_calendar = UserCalendar.find_or_initialize_by_user_id_and_calendar_id(current_refinery_user.id, user_calendar_params[:calendar_id])
         @user_calendar.inactive = user_calendar_params[:inactive]
         if @user_calendar.save
-          render js: { success: true }
+          render json: { success: true }
         else
-          render js: { success: false }
+          render json: { success: false }
         end
       end
 
